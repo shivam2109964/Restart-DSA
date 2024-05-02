291efb5f87ec2d0ee06dbb58c35af897545385c5
shivam_since2002 <sg2109964@gmail.com>
2024-05-02 21:08:39 +0530

    2 May

--- Restart/Complexity/Algorithm/Algorithm.dart
+++ Restart/Complexity/Algorithm/Algorithm.dart
@@ -1,3 +1,5 @@
+//Implementing a method to show how much it take time
+
 int sumFormOneTO(int n) {
   var sum = 0;
   for (var i = 1; i <= n; i++) {
@@ -6,11 +8,18 @@ int sumFormOneTO(int n) {
   return sum;
 }
 
+//One more implementation
+int betterSumFromOneTo(int n) {
+  return n * (n + 1) ~/ 2;
+}
+
 void main() {
   final start = DateTime.now();
-  var result = sumFormOneTO(10000);
+  //var result = sumFormOneTO(10000);
+  var result1 = betterSumFromOneTo(10000);
   final end = DateTime.now();
   final time = end.difference(start);
-  print(result); 
+  //print(result);
+  print(result1);
   print("Time is $time");
 }
